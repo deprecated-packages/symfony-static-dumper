aca0dbe63fd23ae0f369452267b7cf94fd7e96f8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-01 00:18:05 +0700

    [CS] Fixes #2277 Fixes false positive CheckUnneededSymfonyStyleUsageRule  (#2281)
    * [CS] Fixes #2277 Fixes false positive CheckUnneededSymfonyStyleUsageRule
    
    * [CS] Fixes #2277 Fixes false positive CheckUnneededSymfonyStyleUsageRule
    
    * cs fix

---
+++ src/Routing/RoutesProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\SymfonyStaticDumper\Routing;
+
+use Nette\Utils\Strings;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * @see \Symplify\SymfonyStaticDumper\Tests\Routing\RoutesProviderTest
+ */
+final class RoutesProvider
+{
+    /**
+     * @var string
+     */
+    private const PARAMETERS_IN_ROUTE_REGEX = '#\{(.*?)\}#sm';
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @return Route[]
+     */
+    public function provide(): array
+    {
+        return $this->router->getRouteCollection()
+            ->all();
+    }
+
+    /**
+     * @return Route[]
+     */
+    public function provideRoutesWithoutArguments(): array
+    {
+        return array_filter($this->provide(), function (Route $route): bool {
+            return ! $this->hasRouteParameters($route);
+        });
+    }
+
+    /**
+     * @return Route[]
+     */
+    public function provideRoutesWithParameters(): array
+    {
+        return array_filter($this->provide(), function (Route $route): bool {
+            return $this->hasRouteParameters($route);
+        });
+    }
+
+    private function hasRouteParameters(Route $route): bool
+    {
+        return (bool) Strings::match($route->getPath(), self::PARAMETERS_IN_ROUTE_REGEX);
+    }
+}
