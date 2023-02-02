9cafb734f65f51610b3c0121cb1158edc0a4e42f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-02 12:02:00 +0700

    clean up easy-ci config

--- easy-ci.php
+++ easy-ci.php
@@ -6,8 +6,5 @@ use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
-        \Symplify\ConfigTransformer\NodeVisitor\RefOrServiceFuncCallPrePrintNodeVisitor::class,
-        \Symplify\ConfigTransformer\Console\ConfigTransformerApplication::class,
-        \Symplify\ConfigTransformer\Kernel\ConfigTransformerKernel::class,
     ]);
 };
